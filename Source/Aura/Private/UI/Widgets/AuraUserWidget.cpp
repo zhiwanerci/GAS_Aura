bb1ea0d578c2972735715a6e42b4ef700db5bfc6
zhiwanerci <1246695901@qq.com>
2024-09-06 11:31:02 +0800

    Add SuraWidgetController.h/.cpp and AuraUserWidget.h/.cpp

---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// copyright qileisong
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
