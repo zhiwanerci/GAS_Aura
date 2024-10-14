5f86cab54537ce9385e6a7a809b1ec9e6f7760e4
zhiwanerci <1246695901@qq.com>
2024-10-14 18:48:11 +0800

    gameplaytags singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,22 @@
+// copyright qileisong
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ * 
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	static void InitializeNativeGameplayTags();
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
