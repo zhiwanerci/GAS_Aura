075d18104c662d0c17d32de87726d447de679c4a
zhiwanerci <1246695901@qq.com>
2024-10-16 11:52:42 +0800

    UAttributeMenuWidgetController

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// copyright qileisong
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
