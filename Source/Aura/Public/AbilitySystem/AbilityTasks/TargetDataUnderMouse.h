52203bae00e37d43ddfd65a590383008b495b9d4
zhiwanerci <1246695901@qq.com>
2024-10-21 17:12:08 +0800

    Receiving target data

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -29,4 +29,6 @@ private:
 
 	virtual void Activate() override;
 	void SendMouseCursorData();
+
+	void OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle, FGameplayTag ActivationTag);
 };
