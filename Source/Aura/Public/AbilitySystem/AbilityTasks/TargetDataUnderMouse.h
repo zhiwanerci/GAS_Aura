b0310d9827fd69ec6e132083a496d974fdb5843b
zhiwanerci <1246695901@qq.com>
2024-10-21 12:30:40 +0800

    send mousecursordata

--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -6,7 +6,7 @@
 #include "Abilities/Tasks/AbilityTask.h"
 #include "TargetDataUnderMouse.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, Data);
 
 /**
  * 
@@ -28,4 +28,5 @@ public:
 private:
 
 	virtual void Activate() override;
+	void SendMouseCursorData();
 };
