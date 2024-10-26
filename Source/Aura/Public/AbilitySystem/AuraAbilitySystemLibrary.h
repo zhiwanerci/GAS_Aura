93a6406dcefac77b4ea9cf4e34e110efb4ee72a8
zhiwanerci <1246695901@qq.com>
2024-10-26 14:12:21 +0800

    伤害计算系数CurveTable

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -30,4 +30,8 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|CharacterClassDefaults")
 	static void GiveStartupAbilities(const UObject* WorldContextObject, UAbilitySystemComponent* ASC);
+
+
+	UFUNCTION(BlueprintCallable, Category = "AuraAbilitySystemLibrary|CharacterClassDefaults")
+	static UCharacterClassInfo* GetCharacterClassInfo(const UObject* WorldContextObject);
 };
