10b73e4d8ebe0e3bd08608eedccc069e7f5dc713
zhiwanerci <1246695901@qq.com>
2024-10-26 16:14:11 +0800

    Add     AuraAbilitySystemGlobals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,9 @@
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
