2f009cbdf2b67229d088d24997c60384fa12456a
zhiwanerci <1246695901@qq.com>
2024-11-01 18:08:00 +0800

    New Enemy - Demon

--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -64,6 +64,9 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Combat")
 	FName RightHandSocketName;
 
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	FName TailSocketName;
+
 	bool bDead = false;
 
 	UPROPERTY()
